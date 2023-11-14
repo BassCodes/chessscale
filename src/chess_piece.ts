a3f200626284a101adc1b630a209cfade6be42cd
Dalkson <26678089+Dalkson@users.noreply.github.com>
2023-11-14 18:43:52 -0500

    movement, needs refactor but it works

--- src/chess_piece.ts
+++ src/chess_piece.ts
@@ -16,11 +16,11 @@ export abstract class ChessPiece {
 		throw new Error("unimplemented");
 	}
 	// Points in which the piece can move relative to the current point
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		throw new Error("unimplemented");
 	}
 	// Points in which the piece can capture (if there is a piece) relative to the current point
-	getCaptures(): Array<Point> {
+	getCaptures(): Array<Array<Point>> {
 		// By default, the piece can capture in any place it can move (assuming there is a piece in that location)
 		return this.getMoves();
 	}
@@ -28,127 +28,127 @@ export abstract class ChessPiece {
 
 export class Pawn extends ChessPiece {
 	firstMove: boolean = true;
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		const PAWN_MOVEMENT: Array<Point> = [(this.color === ChessPieceColor.White) ? [0, -1] : [0, 1]];
 		if (this.firstMove) {
 			PAWN_MOVEMENT.push((this.color === ChessPieceColor.White) ? [0, -2] : [0, 2]);
 		}
-		return PAWN_MOVEMENT;
+		return [PAWN_MOVEMENT];
 	}
-	getCaptures(): Array<Point> {
-		return (this.color === ChessPieceColor.White) ?  [[-1, -1], [1, -1],] : [[-1, 1], [1, 1],];
+	getCaptures(): Array<Array<Point>> {
+		return (this.color === ChessPieceColor.White) ?  [[[-1, -1], [1, -1],]] : [[[-1, 1], [1, 1],]];
 	}
 	getImage(): Image { 
 		return (this.color === ChessPieceColor.White) ? TextureStore.pieces[5] : TextureStore.pieces[11];
 	}
 }
 
-const ADJACENT_PIECES: Array<Point> = [
-	[1, 1],
-	[1, 0],
-	[1, -1],
-	[0, 1],
-	[0, -1],
-	[-1, 1],
-	[-1, 0],
-	[-1, -1],
+const ADJACENT_PIECES: Array<Array<Point>> = [
+	[[1, 1],],
+	[[1, 0],],
+	[[1, -1],],
+	[[0, 1],],
+	[[0, -1],],
+	[[-1, 1],],
+	[[-1, 0],],
+	[[-1, -1],],
 ];
 
-const DIAGONAL: Array<Point> = [
+const DIAGONAL: Array<Array<Point>> = [
 	// Up Left
-	[-1, -1],
-	[-2, -2],
-	[-3, -3],
-	[-4, -4],
-	[-5, -5],
-	[-6, -6],
-	[-7, -7],
-	[-8, -8],
+	[[-1, -1],
+		[-2, -2],
+		[-3, -3],
+		[-4, -4],
+		[-5, -5],
+		[-6, -6],
+		[-7, -7],
+		[-8, -8],],
 	// Up Right
-	[1, -1],
-	[2, -2],
-	[3, -3],
-	[4, -4],
-	[5, -5],
-	[6, -6],
-	[7, -7],
-	[8, -8],
+	[[1, -1],
+		[2, -2],
+		[3, -3],
+		[4, -4],
+		[5, -5],
+		[6, -6],
+		[7, -7],
+		[8, -8],],
 	// Down Left
-	[-1, 1],
-	[-2, 2],
-	[-3, 3],
-	[-4, 4],
-	[-5, 5],
-	[-6, 6],
-	[-7, 7],
-	[-8, 8],
+	[[-1, 1],
+		[-2, 2],
+		[-3, 3],
+		[-4, 4],
+		[-5, 5],
+		[-6, 6],
+		[-7, 7],
+		[-8, 8],],
 	// Down Right
-	[1, 1],
-	[2, 2],
-	[3, 3],
-	[4, 4],
-	[5, 5],
-	[6, 6],
-	[7, 7],
-	[8, 8],
+	[[1, 1],
+		[2, 2],
+		[3, 3],
+		[4, 4],
+		[5, 5],
+		[6, 6],
+		[7, 7],
+		[8, 8],],
 ];
 
-const VERTICAL_HORIZONTAL: Array<Point> = [
+const VERTICAL_HORIZONTAL: Array<Array<Point>> = [
 	// Right
-	[1, 0],
-	[2, 0],
-	[3, 0],
-	[4, 0],
-	[5, 0],
-	[6, 0],
-	[7, 0],
-	[8, 0],
+	[[1, 0],
+		[2, 0],
+		[3, 0],
+		[4, 0],
+		[5, 0],
+		[6, 0],
+		[7, 0],
+		[8, 0],],
 	// Left
-	[-1, 0],
-	[-2, 0],
-	[-3, 0],
-	[-4, 0],
-	[-5, 0],
-	[-6, 0],
-	[-7, 0],
-	[-8, 0],
+	[[-1, 0],
+		[-2, 0],
+		[-3, 0],
+		[-4, 0],
+		[-5, 0],
+		[-6, 0],
+		[-7, 0],
+		[-8, 0],],
 	// Up
-	[0, -1],
-	[0, -2],
-	[0, -3],
-	[0, -4],
-	[0, -5],
-	[0, -6],
-	[0, -7],
-	[0, -8],
+	[[0, -1],
+		[0, -2],
+		[0, -3],
+		[0, -4],
+		[0, -5],
+		[0, -6],
+		[0, -7],
+		[0, -8],],
 	// Down
-	[0, 1],
-	[0, 2],
-	[0, 3],
-	[0, 4],
-	[0, 5],
-	[0, 6],
-	[0, 7],
-	[0, 8],
+	[[0, 1],
+		[0, 2],
+		[0, 3],
+		[0, 4],
+		[0, 5],
+		[0, 6],
+		[0, 7],
+		[0, 8],],
 ];
 
-const KNIGHT_MOVEMENT: Array<Point> = [
-	[2, 1],
-	[2, -1],
-	[-2, 1],
-	[-2, -1],
-	[1, 2],
-	[1, -2],
-	[-1, 2],
-	[-1, -2],
+const KNIGHT_MOVEMENT: Array<Array<Point>> = [
+	[[2, 1],],
+	[[2, -1],],
+	[[-2, 1],],
+	[[-2, -1],],
+	[[1, 2],],
+	[[1, -2],],
+	[[-1, 2],],
+	[[-1, -2],],
 ];
 
-const VERTICAL_HORIZONTAL_DIAGONAL: Array<Point> = [];
+const VERTICAL_HORIZONTAL_DIAGONAL: Array<Array<Point>> = [];
 VERTICAL_HORIZONTAL_DIAGONAL.push(...VERTICAL_HORIZONTAL);
 VERTICAL_HORIZONTAL_DIAGONAL.push(...DIAGONAL);
 
 export class King extends ChessPiece {
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		return ADJACENT_PIECES;
 	}
 	getImage(): Image { 
@@ -158,7 +158,7 @@ export class King extends ChessPiece {
 
 // TODO add behavior for other pieces
 export class Queen extends ChessPiece {
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		return VERTICAL_HORIZONTAL_DIAGONAL;
 	}
 	getImage(): Image {
@@ -167,7 +167,7 @@ export class Queen extends ChessPiece {
 }
 
 export class Rook extends ChessPiece {
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		return VERTICAL_HORIZONTAL;
 	}
 	getImage(): Image {
@@ -176,7 +176,7 @@ export class Rook extends ChessPiece {
 }
 
 export class Bishop extends ChessPiece {
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		return DIAGONAL;
 	}
 	getImage(): Image {
@@ -185,7 +185,7 @@ export class Bishop extends ChessPiece {
 }
 
 export class Knight extends ChessPiece {
-	getMoves(): Array<Point> {
+	getMoves(): Array<Array<Point>> {
 		return KNIGHT_MOVEMENT;
 	}
 	getImage(): Image {
--- src/ui.ts
+++ src/ui.ts
@@ -1,6 +1,6 @@
 import Camera from "./camera";
 import ChessBoard from "./chess_board";
-import { ChessPiece, Pawn } from "./chess_piece";
+import { ChessPiece, ChessPieceColor, Pawn } from "./chess_piece";
 import { TILE_SIZE } from "./constants";
 import { Point, addPoint, eqPoint, mulPoint } from "./lib/util";
 
@@ -8,7 +8,7 @@ import { Point, addPoint, eqPoint, mulPoint } from "./lib/util";
 export default class UI {
 	selectedPosition: null | Point = null;
 	selectedPiece: null | ChessPiece = null;
-
+	moves: Array<Point> = [];
 
 	clickBoard(x: number, y: number, board: ChessBoard, key: string): void {
 		// TODO: mega refactor this
@@ -20,6 +20,7 @@ export default class UI {
 		if (key === "up" && this.selectedPosition === null) {
 			this.selectedPosition = null;
 			this.selectedPiece = null;
+			
 			return;
 		}
 
@@ -29,7 +30,7 @@ export default class UI {
 			return;
 		}
 
-		if (key === "down" && board.getPiece(...clicked) !== null) {
+		if (key === "down" && board.getPiece(...clicked)?.color === ChessPieceColor.White) {
 			this.selectedPosition = clicked;
 			this.selectedPiece = board.getPiece(...clicked);
 			return;
@@ -38,17 +39,34 @@ export default class UI {
 		if (this.selectedPiece === null || this.selectedPosition === null) {
 			return;
 		}
-
-		for (const move of this.selectedPiece.getMoves()) {
-			const updatedPosition = addPoint(move, this.selectedPosition);
-			if (eqPoint(updatedPosition, clicked)) {
-				board.setPiece(...updatedPosition, this.selectedPiece);
+		
+		this.moves = [];
+		for (const rays of this.selectedPiece.getMoves()) {
+			for (const move of rays) {
+				const updatedPosition = addPoint(move, this.selectedPosition);
+				const updatedPostitionPiece = board.getPiece(...updatedPosition);
+				if (updatedPostitionPiece !== null) {
+					if (this.selectedPiece.color === updatedPostitionPiece?.color) {
+						break;
+					} else {
+						this.moves.push(updatedPosition);
+						break;
+					}
+				}
+				this.moves.push(updatedPosition);
+			}
+		}
+		
+		for(const move of this.moves) {
+			if (eqPoint(move, clicked)) {
+				board.setPiece(...move, this.selectedPiece);
 				board.setPiece(...this.selectedPosition, null);
 				if (this.selectedPiece instanceof Pawn) {
 					this.selectedPiece.firstMove = false;
 				}
 				this.selectedPosition = null;
 				this.selectedPiece = null;
+				this.moves = [];
 				return;
 			}
 		}
@@ -58,6 +76,7 @@ export default class UI {
 		}
 		this.selectedPosition = null;
 		this.selectedPosition = null;
+		this.moves = [];
 	}
 
 	drawMovements(cam: Camera, board: ChessBoard): void {
@@ -71,13 +90,29 @@ export default class UI {
 			throw new Error("unreachable code executed");
 		}
 		cam.ctx.strokeStyle = "green";
-		for (const move of piece.getMoves()) {
-			const position = addPoint(this.selectedPosition, move);
+		for (const move of this.moves) {
+			const positionPiece = board.getPiece(...move);
 			// TODO: maybe a more visible color?
+			if (positionPiece !== null) {
+				if (piece.color === positionPiece?.color) {
+					break;
+				} else {
+					cam.ctx.fillStyle = "rgba(140,140,140,0.6)";
+					cam.ctx.beginPath();
+					cam.ctx.arc(
+						...addPoint(mulPoint(move, TILE_SIZE), 0.5 * TILE_SIZE),
+						TILE_SIZE / 6,
+						0,
+						Math.PI * 2
+					);
+					cam.ctx.fill();
+					break;
+				}
+			}
 			cam.ctx.fillStyle = "rgba(140,140,140,0.6)";
 			cam.ctx.beginPath();
 			cam.ctx.arc(
-				...addPoint(mulPoint(position, TILE_SIZE), 0.5 * TILE_SIZE),
+				...addPoint(mulPoint(move, TILE_SIZE), 0.5 * TILE_SIZE),
 				TILE_SIZE / 6,
 				0,
 				Math.PI * 2
